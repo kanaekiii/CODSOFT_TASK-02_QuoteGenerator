599c68bd787c2e69e60bb7577b593dd53e80ce5f
Abinaya Raghuraman <111069912+kanaekiii@users.noreply.github.com>
2023-09-09 00:21:35 +0530

    Initial Commit

---
+++ app/src/main/java/com/codsoft/quotegenerator/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.codsoft.quotegenerator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Animation fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        final View layout = findViewById(R.id.splash);
+
+        layout.startAnimation(fadeAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
